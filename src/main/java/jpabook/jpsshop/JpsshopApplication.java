df9f97c721644a55b8145537cbede473230412dd
jang010505 <jang010505@inu.ac.kr>
2023-07-15 17:20:46 +0900

    first commit

---
+++ src/main/java/jpabook/jpsshop/JpsshopApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpsshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpsshopApplication.class, args);
+	}
+
+}
