0e1198ec418e0e10649a37663269135dccc76bbf
jang010505 <jang010505@inu.ac.kr>
2023-07-15 17:27:12 +0900

    docs: Rename package

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,13 +1,13 @@
-package jpabook.jpsshop;
+package jpabook.jpashop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpsshopApplication {
+public class JpashopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpsshopApplication.class, args);
+		SpringApplication.run(JpashopApplication.class, args);
 	}
 
 }
